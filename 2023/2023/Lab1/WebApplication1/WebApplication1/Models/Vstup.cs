f5c7e3821666d567f7725670eee272051f6095f3
Sofia <93678956+SofiaMudrenok@users.noreply.github.com>
2023-04-09 11:57:15 +0300

    lab1

---
+++ 2023/2023/Lab1/WebApplication1/WebApplication1/Models/Vstup.cs
@@ -0,0 +1,13 @@
+﻿
+namespace WebApplication1.Models
+{
+    public class Vstup
+    {
+        public int Id { get; set; }
+        public string? FullName { get; set; }
+        public int? Year { get; set; }
+        public int? ZNO { get; set; }
+        public string? Sex { get; set; }
+
+    }
+}
