c838d2e95ec3def873806dc7b0f4de3cc333df2a
Sofia <93678956+SofiaMudrenok@users.noreply.github.com>
2023-04-09 13:22:46 +0300

    lab

---
+++ 2023/2023/lab3/WebApplication1/WebApplication1/Data/VstupContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class VstupContext : DbContext
+    {
+        public VstupContext(DbContextOptions<VstupContext> options) : base(options) { }
+
+        public DbSet<Vstup> Vstup { get; set; }
+    }
+}
